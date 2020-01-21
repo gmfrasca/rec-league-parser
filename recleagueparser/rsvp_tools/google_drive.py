78213db4a4ccc35d308940e3541087a1a6e3877d
Giulio <giulio.m.frasca@gmail.com>
2020-01-21 02:57:11 -0500

    Add Google Doc RSVP Parser

---
+++ recleagueparser/rsvp_tools/google_drive.py
@@ -0,0 +1,111 @@
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.http import MediaIoBaseDownload
+from google.auth.transport.requests import Request
+from googleapiclient.discovery import build
+from recleagueparser.rsvp_tools.rsvp_tool import RsvpTool
+import os.path
+import logging
+import pickle
+import re
+import io
+
+
+ENTRY_REGEX = r'^[\*-]\s+.*$'
+REMOVE_REGEX = r'^[\*-]\s+'
+PICKLE_LOCATION = './gcp.token.pickle'
+SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
+
+
+class GoogleDriveSignupSheet(RsvpTool):
+
+    def __init__(self, cred_file, file_id, skip_lists=0, **kwargs):
+        self._logger = logging.getLogger(self.__class__.__name__)
+        self.cred_file = cred_file
+        self.skip_lists = skip_lists
+        self.file_id = file_id
+
+    @property
+    def credentials(self):
+        creds = None
+        if os.path.exists(PICKLE_LOCATION):
+            with open(PICKLE_LOCATION, 'rb') as token:
+                creds = pickle.load(token)
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    self.cred_file, SCOPES)
+                creds = flow.run_local_server(port=0)
+            with open(PICKLE_LOCATION, 'wb') as token:
+                pickle.dump(creds, token)
+        return creds
+
+    def login(self):
+        self.schedule = self.parse_file()
+
+    def retrieve_text(self):
+        service = build('drive', 'v3', credentials=self.credentials)
+        request = service.files().export_media(fileId=self.file_id,
+                                               mimeType='text/plain')
+        fh = io.BytesIO()
+        downloader = MediaIoBaseDownload(fh, request)
+        done = False
+        while done is False:
+            status, done = downloader.next_chunk()
+        return fh.getvalue().decode('utf-8')
+
+    def parse_file(self):
+        text = self.retrieve_text()
+        lists = []
+        prev_line = ''
+        in_list = False
+        title = ''
+        players = []
+
+        for line in text.splitlines():
+            current = line.strip()
+            if re.match(ENTRY_REGEX, current):
+                if not in_list:
+                    in_list = True
+                    title = prev_line
+                players.append(re.sub(REMOVE_REGEX, '', current))
+            elif in_list:
+                in_list = False
+                lists.append({'title': title, 'players': players.copy()})
+                players = []
+            prev_line = current
+        if in_list:
+            lists.append({'title': title, 'players': players.copy()})
+        return lists
+
+    def get_game(self, game_id):
+        self.login()
+        game_id = game_id + self.skip_lists
+        return {} if game_id >= len(self.schedule) else self.schedule[game_id]
+
+    def get_outage_list(self):
+        self.login()
+        if self.skip_lists == 0 or len(self.schedule) == 0:
+            return {}
+        return self.schedule[0]
+
+    def get_next_game(self):
+        return self.get_game(0)
+
+    # TODO: Refactor to get_next_game_attendance_str
+    def get_next_game_attendance(self):
+        return 'In: {}, Out: {}'.format(
+            len(self.get_next_game_players_list()),
+            len(self.get_outage_list()))
+
+    def get_outage_players_list(self):
+        return self.get_outage_list().get('players', [])
+
+    def get_next_game_attendees(self):
+        attendees = ', '.join(self.get_next_game_players_list())
+        outage = ', '.join(self.get_outage_players_list())
+        return 'In: {},\r\nOut: {}'.format(attendees, outage)
+
+    def get_next_game_players_list(self):
+        return self.get_next_game().get('players', [])
